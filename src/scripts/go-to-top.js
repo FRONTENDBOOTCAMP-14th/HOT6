79a08268c72dd0a1f58bda2431c43a6025f9888a
mjnji-kim0524 <always0524@naver.com>
2025-07-29 15:09:41 +0900

    [Refactor] 맨위로버튼 js 기능 클래스명 hook 적용

--- src/scripts/go-to-top.js
+++ src/scripts/go-to-top.js
@@ -1,22 +1,7 @@
 
   const topButton = document.querySelector('.goToTopButton')
-  const bookListSectionHome = document.querySelector('.bookListWrapperHome')
-  const bookListSectionFav = document.querySelector('.bookListWrapper')
-  const bookListSectionMainMobile = document.querySelector('.bookListWrapperMobile')
-  const bookListSectionMainPC = document.querySelector('.bookListWrapperPC')
+  const bookListWrapper = document.querySelector('.jsbookListWrapper')
 
   topButton.addEventListener('click', () => {
-    bookListSectionHome.scrollTop = 0
-  })
-
-  topButton.addEventListener('click', () => {
-    bookListSectionFav.scrollTop = 0
-  })
-
-  topButton.addEventListener('click', () => {
-    bookListSectionMainMobile.scrollTop = 0
-  })
-
-  topButton.addEventListener('click', () => {
-    bookListSectionMainPC.scrollTop = 0
+    bookListWrapper.scrollTop = 0
   })
