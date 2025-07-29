f0d0c27a045664204644b5c572b35a465a035fa6
yoonseo23 <jysjdt03gmail.com>
2025-07-29 13:59:45 +0900

    [Feat] 입력값 삭제 기능 구현 - main-page에서 두 요소 모두 찾을 수 있도록 querySelectorAll, forEach문으로 재작성 - 취소버튼 클릭시 입력값 삭제 및 입력란에 포커스 부여 - js 코드 전문 스코프로 프레이밍화

--- src/scripts/clear-button.js
+++ src/scripts/clear-button.js
@@ -1,21 +1,27 @@
-const searchBar = document.querySelector('.searchBar')
-const form = searchBar.querySelector('form')
-const clearButton = form.querySelector('.clearButton')
+{
+  const searchBars = document.querySelectorAll('.jsSearchBar');
 
-/**
- * Webkit 브라우저 여부를 확인하는 식을 반환하는 함수
- */
-function isWebkit() {
-  return /AppleWebKit/.test(navigator.userAgent)
+  searchBars.forEach((searchBar) => {
+    const form = searchBar.querySelector('form');
+    const clearButton = form.querySelector('.clearButton');
+
+    form.addEventListener('input', () => {
+      const input = form.querySelector('input');
+      if (isWebkit()) return;
+      clearButton.style.display = input.value ? 'block' : 'none';
+
+      clearButton.addEventListener('click', () => {
+        input.value = '';
+        clearButton.style.display = 'none';
+        input.focus();
+      });
+    });
+  });
+
+  /**
+   * Webkit 브라우저 여부를 확인하는 식을 반환하는 함수
+   */
+  function isWebkit() {
+    return /AppleWebKit/.test(navigator.userAgent);
+  }
 }
-
-form.addEventListener('input', () => {
-  const input = form.querySelector('input')
-  if (isWebkit()) return
-  clearButton.style.display = input.value ? 'block' : 'none'
-
-  clearButton.addEventListener('click', () => {
-    input.value = ''
-    clearButton.style.display = 'none'
-  })
-})
