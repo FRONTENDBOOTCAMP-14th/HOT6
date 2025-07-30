efb9ceaca83b087f54b56e884d8f6fd01a5238d5
cjftns <bimax1234@naver.com>
2025-07-30 17:45:34 +0900

    [Chore] 맨 위로 버튼 추가, 주석 추가, 이미지 경로 변경

--- src/scripts/author.js
+++ src/scripts/author.js
@@ -3,84 +3,84 @@ const authors = [
     name: '한강',
     displayName: 'Han Kang',
     wiki: 'https://namu.wiki/w/한강(소설가)',
-    img: 'author_Han_Kang',
+    img: 'Han_Kang',
     nationality: 'korean',
   },
   {
     name: '김금희',
     displayName: 'Kim Keum-Hee',
     wiki: 'https://namu.wiki/w/김금희',
-    img: 'author_Kim_Keum_Hee',
+    img: 'Kim_Keum_Hee',
     nationality: 'korean',
   },
   {
     name: '김애란',
     displayName: 'Kim Ae-Ran',
     wiki: 'https://namu.wiki/w/김애란',
-    img: 'author_Kim_Ae_Ran',
+    img: 'Kim_Ae_Ran',
     nationality: 'korean',
   },
   {
     name: '김영하',
     displayName: 'Kim Young-Ha',
     wiki: 'https://namu.wiki/w/김영하',
-    img: 'author_Kim_Young_Ha',
+    img: 'Kim_Young_Ha',
     nationality: 'korean',
   },
   {
     name: '성해나',
     displayName: 'Sung Hae-Na',
     wiki: 'https://namu.wiki/w/성해나',
-    img: 'author_Sung_Hae_Na',
+    img: 'Sung_Hae_Na',
     nationality: 'korean',
   },
   {
     name: '황석영',
     displayName: 'Hwang Sok-Yong',
     wiki: 'https://namu.wiki/w/황석영',
-    img: 'author_Hwang_Sok_yong',
+    img: 'Hwang_Sok_yong',
     nationality: 'korean',
   },
   {
     name: '앙투안 드 생택쥐페리',
     displayName: 'Antoine de Saint-Exupery',
     wiki: 'https://namu.wiki/w/앙투안%20드%20생텍쥐페리',
-    img: 'author_Saint_Exupery',
+    img: 'Saint_Exupery',
     nationality: 'international',
   },
   {
     name: '어니스트 헤밍웨이',
     displayName: 'Ernest Miller Hemingway',
     wiki: 'https://namu.wiki/w/어니스트%20헤밍웨이',
-    img: 'author_Hemingway',
+    img: 'Hemingway',
     nationality: 'international',
   },
   {
     name: 'J.K.롤링',
     displayName: 'J.K. Rowling',
     wiki: 'https://namu.wiki/w/J.%20K.%20롤링',
-    img: 'author_Joanne_K',
+    img: 'Joanne_K',
     nationality: 'international',
   },
   {
     name: '헤르만 헤세',
     displayName: 'Hermann Hesse',
     wiki: 'https://namu.wiki/w/헤르만%20헤세',
-    img: 'author_Herman_Hesse',
+    img: 'Herman_Hesse',
     nationality: 'international',
   },
   {
     name: '레프 톨스토이',
     displayName: 'Leo Tolstoy',
     wiki: 'https://namu.wiki/w/레프%20톨스토이',
-    img: 'author_Tolstoy',
+    img: 'Tolstoy',
     nationality: 'international',
   },
   {
     name: '베르나르 베르베르',
     displayName: 'Bernard Werber',
     wiki: 'https://namu.wiki/w/베르나르%20베르베르',
-    img: 'author_Bernard_Werber',
+    img: 'Bernard_Werber',
     nationality: 'international',
   },
 ];
@@ -111,11 +111,11 @@ function renderAuthors(list, containerId) {
         <figure>
           <picture>
             <source type="image/webp"
-              srcset="../../assets/images/author/${author.img}_webp.webp 1x,
-                      ../../assets/images/author/${author.img}_webp_2x.webp 2x" />
-            <img src="../../assets/images/author/${author.img}.png"
-                 srcset="../../assets/images/author/${author.img}.png 1x,
-                         ../../assets/images/author/${author.img}_2x.png 2x"
+              srcset="/src/assets/images/author/author_${author.img}_webp.webp 1x,
+                      /src/assets/images/author/author_${author.img}_webp_2x.webp 2x" />
+            <img src="/src/assets/images/author/author_${author.img}.png"
+                 srcset="/src/assets/images/author/author_${author.img}.png 1x,
+                         /src/assets/images/author/author_${author.img}_2x.png 2x"
                  alt="${author.name}의 나무위키로 이동" />
           </picture>
           <figcaption>${author.displayName}</figcaption>
