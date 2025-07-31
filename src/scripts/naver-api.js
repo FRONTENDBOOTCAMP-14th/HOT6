c6f745a49448a113b9b4dc1fb13a9fcd484a938f
dohyeon0709 <dohy0709@naver.com>
2025-07-31 23:11:00 +0900

    [Refactor] search-book 함수 분리 및 naver-api 코드 정리

--- src/scripts/naver-api.js
+++ src/scripts/naver-api.js
@@ -1,11 +1,8 @@
 import DOMPurify from '../modules/dompurify';
 import { createBookModal, openBookModal, modalClose } from '../utils/modal';
-import { createCard } from '../utils/create-card';
+import { searchBooks } from '../utils/search-book';
 import { getFavoriteBookFromCard } from '../utils/get-favorite-book-from-card';
 import { getFavoriteBookFromModal } from '../utils/get-favorite-book-from-modal';
-
-const clientId = import.meta.env.VITE_NAVER_CLIENT_ID;
-const clientSecret = import.meta.env.VITE_NAVER_CLIENT_SECRET;
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.searchBar form');
@@ -15,12 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const countElement = document.getElementById('searchBookCount');
   const nameElement = document.getElementById('searchBookName');
 
-  let cleanQuery = '';
+  const queryVariables = {
+    cleanQuery: '',
+    start: 1,
+    display: 12,
+    isLoading: false,
+    moreBooks: true,
+  };
+
   let query = '';
-  let start = 1;
-  const display = 12;
-  let isLoading = false;
-  let moreBooks = true;
 
   const modal = createBookModal();
 
@@ -28,12 +28,19 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     query = input.value.trim();
-    cleanQuery = DOMPurify.sanitize(query);
-    if (!cleanQuery) return alert('검색어 입력은 필수입니다.');
+    queryVariables.cleanQuery = DOMPurify.sanitize(query);
+    if (!queryVariables.cleanQuery) return alert('검색어 입력은 필수입니다.');
 
-    start = 1;
-    moreBooks = true;
-    searchBooks(true);
+    queryVariables.start = 1;
+    queryVariables.moreBooks = true;
+    searchBooks(
+      queryVariables,
+      true,
+      resultContainer,
+      countElement,
+      nameElement,
+      searchBookComment
+    );
   });
 
   resultContainer.addEventListener('click', (e) => {
@@ -85,54 +92,23 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // 무한 스크롤
-
   resultContainer.addEventListener('scroll', () => {
     const scrollTop = resultContainer.scrollTop;
     const scrollHeight = resultContainer.scrollHeight;
     const clientHeight = resultContainer.clientHeight;
-    if (scrollTop + clientHeight >= scrollHeight - 100 && !isLoading && moreBooks) {
-      searchBooks();
+    if (
+      scrollTop + clientHeight >= scrollHeight - 100 &&
+      !queryVariables.isLoading &&
+      queryVariables.moreBooks
+    ) {
+      searchBooks(
+        queryVariables,
+        false,
+        resultContainer,
+        countElement,
+        nameElement,
+        searchBookComment
+      );
     }
   });
-
-  /**
-   * 도서 검색 함수
-   * @param {*} isNewSearch 첫 검색인지 추가 검색인지 구분해줌
-   * @returns
-   */
-  function searchBooks(isNewSearch = false) {
-    if (isLoading || !moreBooks) return;
-    isLoading = true;
-    fetch(
-      `/api/v1/search/book.json?query=${encodeURIComponent(cleanQuery)}&start=${start}&display=${display}`,
-      {
-        headers: {
-          'X-Naver-Client-Id': clientId,
-          'X-Naver-Client-Secret': clientSecret,
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (isNewSearch) {
-          resultContainer.innerHTML = '';
-          // 결과 텍스트
-          countElement.textContent = data.total;
-          nameElement.textContent = query;
-        }
-        if (data.items.length === 0) {
-          searchBookComment.innerHTML = `<p>검색 결과가 없습니다.</p>`;
-          moreBooks = false;
-          return;
-        }
-
-        data.items.forEach((item) => createCard(resultContainer, item));
-
-        start += display;
-        moreBooks = data.total > start;
-      })
-      .finally(() => {
-        isLoading = false;
-      });
-  }
 });
