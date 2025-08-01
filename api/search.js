5190b40f36d706618854b68246345d69676b7b20
mjnji-kim0524 <always0524@naver.com>
2025-08-02 02:07:03 +0900

    [Test] 배포 api 오류 해결을 위한 테스트

--- api/search.js
+++ api/search.js
@@ -15,6 +15,6 @@ export default async function handler(req, res) {
     return res.status(response.status).json({ error: 'Failed to fetch from Naver API' });
   }
 
-  const data = await response.json();
+  const data = await response.text();
   res.status(200).json(data);
 }
