d98624557d711cc4dfe04cefc878bc257090bb55
mjnji-kim0524 <always0524@naver.com>
2025-08-01 10:05:37 +0900

    [Docs] naver-api.js 파일명 변경(home.js) 반영

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-
 import '../src/main.css';
+
 // 검색 바(취소 버튼)
 import './scripts/clear-button.js'
-// 네이버 도서 검색 API (검색 함수)
-import './scripts/naver-api.js';
-
-// 즐겨찾기 목록 렌더링
-import './scripts/render-favorite.js';
 
 // 맨위로 버튼(go-to-top)
 import './scripts/go-to-top.js';
 
+// 네이버 도서 검색 API (검색 함수)
+import './scripts/home.js';
+
+// 즐겨찾기 목록 렌더링
+import './scripts/render-favorite.js';
+
 // 작가 페이지
-// import './scripts/author.js';
+import './scripts/author.js';
